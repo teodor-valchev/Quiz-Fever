9d509bd10e24d8428f4bc2023d581ae8130ac3f0
Teodor Valchev <teo.valxev@gmail.com>
2026-02-06 13:02:52 +0200

    fix: Update import paths in app.js and home.js to use CDN links

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import page from "../node_modules/page/page.mjs";
+import page from "https://unpkg.com/page/page.mjs";
 import { homeView } from "./views/home.js";
 
 const rootElement = document.getElementById("content");
--- src/views/home.js
+++ src/views/home.js
@@ -1,4 +1,4 @@
-import { render, html } from "../../node_modules/lit-html/lit-html.js";
+import { html, render } from "https://unpkg.com/lit-html?module";
 
 const homeTemplate = () => html`
     <section id="welcome">
